1a5ecac9efbcb24407eaa06854f7c37d5d4aea1e
Oleh Tatarynov <131361168+eXocriador@users.noreply.github.com>
2025-06-15 15:55:07 +0300

    Initialize hw5-auth

---
+++ src/routers/auth.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import {
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  refreshUserSessionController,
+} from '../controllers/auth.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
+
+const authRouter = Router();
+
+authRouter.post(
+  '/register',
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerUserController),
+);
+authRouter.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
+);
+authRouter.post('/logout', ctrlWrapper(logoutUserController));
+authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+export default authRouter;
